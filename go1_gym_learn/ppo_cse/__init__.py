47884a747d8f814d4fe374f5d3b06d14a12a4f60
Siddharth Gupta <siddharthg21@gmail.com>
2023-11-17 16:54:13 -0500

    Reducing video and checkpoint record interval

--- go1_gym_learn/ppo_cse/__init__.py
+++ go1_gym_learn/ppo_cse/__init__.py
@@ -47,8 +47,8 @@ class RunnerArgs(PrefixProto, cli=False):
     max_iterations = 1500  # number of policy updates
 
     # logging
-    save_interval = 400  # check for potential saves every this many iterations
-    save_video_interval = 100
+    save_interval = 40  # check for potential saves every this many iterations
+    save_video_interval = 50
     log_freq = 10
 
     # load and resume
